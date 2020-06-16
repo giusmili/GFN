14253bd3db83552ca96e037fce36c2cd11cd8531
Gius <giusmile67@gmail.com>
2020-06-16 17:02:44 +0200

    menu burger

--- js/app.js
+++ js/app.js
@@ -2,11 +2,20 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 				//ancre de pour l'élément
 				let el = document.querySelector('.gelery-head-presentation')
+				let manu_burger = document.getElementsByClassName("burger")[0]
+				let manu_appear = document.querySelector("nav")
 				
 
 				 el.addEventListener("click",(e)=>{
 				 	e.stopPropagation()
 				 		document.location.href="#falls"
+				 })
+
+				 //event menu
+
+				 manu_burger.addEventListener("click",()=>{
+				 		manu_appear.classList.toggle("main-menu-appear")
+
 				 })
 			
 	})
